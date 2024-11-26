33bd803d2318a265cf5bc80dbe0bc9bf3a1add51
GitVanHove <92695604+GitVanHove@users.noreply.github.com>
2024-11-26 20:10:38 +0100

    Added lot's of assets
    Major content addition added first pages and css/html made multiple components.

--- Portfolio_Kevin/src/app/app.component.ts
+++ Portfolio_Kevin/src/app/app.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    HeroComponent,
+    AboutComponent,
+    ResumeComponent,
+    SkillsComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true
 })
 export class AppComponent {
   title = 'Portfolio_Kevin';
---
+++ Portfolio_Kevin/src/app/components/portfolio/portfolio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  imports: [],
+  templateUrl: './portfolio.component.html',
+  styleUrl: './portfolio.component.css',
+  standalone: true 
+})
+export class PortfolioComponent {
+
+}
