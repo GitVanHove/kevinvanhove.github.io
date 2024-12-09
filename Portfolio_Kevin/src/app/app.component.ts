9a48a99ee4e71672e2c2e68e8f4b635137fad296
GitVanHove <92695604+GitVanHove@users.noreply.github.com>
2024-12-09 18:42:26 +0100

    Small content update
    Updated hero, resume and about page. Removed unused components

--- Portfolio_Kevin/src/app/app.component.ts
+++ Portfolio_Kevin/src/app/app.component.ts
@@ -4,7 +4,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { AboutComponent } from './components/about/about.component';
 import { ResumeComponent } from './components/resume/resume.component';
-import { SkillsComponent } from './components/skills/skills.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
@@ -15,7 +14,6 @@ import { PortfolioComponent } from './components/portfolio/portfolio.component';
     HeroComponent,
     AboutComponent,
     ResumeComponent,
-    SkillsComponent,
     PortfolioComponent,
     FooterComponent
   ],
@@ -33,5 +31,7 @@ export class AppComponent implements AfterViewInit {
         mirror: false
       }
     );
+
+    
   }
 }
