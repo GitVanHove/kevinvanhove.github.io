94532ae4ae6c343f9435efd8ac74a54d2341ef5c
GitVanHove <92695604+GitVanHove@users.noreply.github.com>
2024-12-11 17:15:36 +0100

    New component + content
    Added a new skills component and added content to resume, hero, skills and about

--- Portfolio_Kevin/src/app/app.component.ts
+++ Portfolio_Kevin/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import AOS from 'aos';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { SkillsComponent } from './components/skills/skills.component';
 import { AboutComponent } from './components/about/about.component';
 import { ResumeComponent } from './components/resume/resume.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -12,6 +13,7 @@ import { PortfolioComponent } from './components/portfolio/portfolio.component';
   imports: [
     HeaderComponent,
     HeroComponent,
+    SkillsComponent,
     AboutComponent,
     ResumeComponent,
     PortfolioComponent,
--- Portfolio_Kevin/src/app/components/skills/skills.component.ts
+++ Portfolio_Kevin/src/app/components/skills/skills.component.ts
@@ -1,12 +1,24 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css',
   standalone: true
 })
 export class SkillsComponent {
 
+  skillsLeft = [
+    { category: 'Front-end', items: [' JavaScript', 'React.js', 'Angular.js', 'Vue.js'] },
+    { category: 'Back-end', items: [' PHP', 'Laravel', 'C#', '.NET', 'Python', 'Node.js'] },
+    { category: 'Mobile development', items: [' Ionic', 'Kotlin'] }
+  ];
+
+  skillsRight = [
+    { category: 'Software', items: [' Java', 'C#', '.NET'] },
+    { category: 'Database', items: [' MySQL', 'GraphQL'] },
+    { category: 'Hardware', items: [' Arduino'] }
+  ];
 }
